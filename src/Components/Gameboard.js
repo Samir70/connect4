f110c3abb8c28c836bab64ee99d03e32951be0ad
Samir70home <sbetmouni@talktalk.net>
2019-05-13 19:20:46 +0100

    Make board display as connect 4 board

--- src/Components/Gameboard.js
+++ src/Components/Gameboard.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import './Gameboard.css';
 
+const makeTile = (c, i) => {
+    var cl = '';
+    var k = 'sq'+i;
+    c === 'B' ? cl = 'black' : c==='Y' ? cl = 'yellow' : cl = 'red';
+    return <td className={cl} key = {k} ></td>
+}
+
 const Gameboard = (props) => {
-    var boardList = props.pos.map(x => <p>{x}</p>);
+    var board = props.pos.join('').split('').map((x, i) => makeTile(x, i))
 
     return (
         <div>
-          <p>Gameboard!</p>
-          {boardList}
-
           <table className="gameBoard" >
               <tbody>
-                 <tr>
-                     <td class="black"></td>
-                     <td class="yellow"></td>
-                     <td class="red"></td>
-                 </tr>
-                 <tr>
-                     <td class="black"></td>
-                     <td class="yellow"></td>
-                     <td class="red"></td>
-                 </tr>
+                <tr>{board.splice(0, 7)}</tr>                
+                <tr>{board.splice(0, 7)}</tr>                
+                <tr>{board.splice(0, 7)}</tr>                
+                <tr>{board.splice(0, 7)}</tr>                
+                <tr>{board.splice(0, 7)}</tr>                
+                <tr>{board.splice(0, 7)}</tr>                
+                <tr>{board.splice(0, 7)}</tr>                
               </tbody>
           </table>
         </div>
--- src/Modules/BoardHandler.js
+++ src/Modules/BoardHandler.js
@@ -6,5 +6,5 @@ export const startPos = [
     'BBBBBBB',
     'BBBBBBB',
     'BBBBBBB',
-    'BBBBBBB'
+    'BBYRBBB'
 ]

5fbc7dd4e1266266ea081a152bd6fc847f7a9b7e
Samir70home <sbetmouni@talktalk.net>
2019-05-13 18:36:19 +0100

    make different coloured board tiles

--- src/Components/Gameboard.js
+++ src/Components/Gameboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Gameboard.css';
 
 const Gameboard = (props) => {
     var boardList = props.pos.map(x => <p>{x}</p>);
@@ -7,6 +8,21 @@ const Gameboard = (props) => {
         <div>
           <p>Gameboard!</p>
           {boardList}
+
+          <table className="gameBoard" >
+              <tbody>
+                 <tr>
+                     <td class="black"></td>
+                     <td class="yellow"></td>
+                     <td class="red"></td>
+                 </tr>
+                 <tr>
+                     <td class="black"></td>
+                     <td class="yellow"></td>
+                     <td class="red"></td>
+                 </tr>
+              </tbody>
+          </table>
         </div>
     )
 }
